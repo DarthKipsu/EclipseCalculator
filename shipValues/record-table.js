adfdb8183a1a91821e63551d4b0a07585a3d40ca
Verna Koskinen <darth.kipsu@gmail.com>
2014-05-16 19:07:58 +0300

    add race value record keeping table

---
+++ shipValues/record-table.js
@@ -0,0 +1,367 @@
+var terran = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 2,
+        energy: 0,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 1,
+        energy: 0,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: null,
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 0,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 4,
+        energy: 3,
+        computer: 0
+    }
+}
+
+var enemyTerran = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 2,
+        energy: 0,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 1,
+        energy: 0,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: null,
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 0,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 4,
+        energy: 3,
+        computer: 0
+    }
+}
+
+var eridani = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 2,
+        energy: 0,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 1,
+        energy: 0,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: null,
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 1,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 4,
+        energy: 3,
+        computer: 0
+    }
+}
+
+var hydran = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 2,
+        energy: 0,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 1,
+        energy: 0,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: null,
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 0,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 4,
+        energy: 3,
+        computer: 0
+    }
+}
+
+var planta = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 0,
+        energy: 2,
+        computer: 1
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: null,
+        six: 'nuclear drive',
+        initiative: 0,
+        energy: 2,
+        computer: 1
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: 'hull',
+        four: 'nuclear-source',
+        five: null,
+        six: 'ion-cannon',
+        seven: null,
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 2,
+        computer: 1
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 2,
+        energy: 5,
+        computer: 1
+    }
+}
+
+var draco = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 2,
+        energy: 0,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 1,
+        energy: 0,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: null,
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 0,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 4,
+        energy: 3,
+        computer: 0
+    }
+}
+
+var mechanema = {
+    interceptor: {
+        one: null,
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 2,
+        energy: 0,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: null,
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 1,
+        energy: 0,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: null,
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 0,
+        energy: 0,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: null,
+        five: 'electron-computer',
+        initiative: 4,
+        energy: 3,
+        computer: 0
+    }
+}
+
+var orion = {
+    interceptor: {
+        one: 'gauss-shield',
+        two: 'ion cannon',
+        three: 'nuclear source',
+        four: 'nuclear drive',
+        initiative: 3,
+        energy: 1,
+        computer: 0
+    }
+    cruiser: {
+        one: 'hull',
+        two: 'gauss-shield',
+        three: 'ion cannon',
+        four: 'nuclear source',
+        five: 'electron computer',
+        six: 'nuclear drive',
+        initiative: 2,
+        energy: 2,
+        computer: 0
+    }
+    dreadnought: {
+        one: 'ion-cannon',
+        two: 'hull',
+        three: 'gauss-shield',
+        four: 'hull',
+        five: 'nuclear-source',
+        six: 'ion-cannon',
+        seven: 'electron-computer',
+        eight: 'nuclear-drive',
+        initiative: 1,
+        energy: 3,
+        computer: 0
+    }
+    starbase: {
+        one: 'hull',
+        two: 'ion-cannon',
+        three: 'hull',
+        four: 'gauss-shield',
+        five: 'electron-computer',
+        initiative: 5,
+        energy: 3,
+        computer: 0
+    }
+}
