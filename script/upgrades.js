7d167c6ba3bbaf6d4ce3fb0c5b7c3a295b2c5a44
Verna Koskinen <darth.kipsu@gmail.com>
2014-05-16 20:01:33 +0300

    update record table when upgrade is added to ship model

--- script/upgrades.js
+++ script/upgrades.js
@@ -1,18 +1,28 @@
 $(document).on('click', '.tech > img', function() {
     var upgradeSrc = this.src
-    $('.tech > img').css({'opacity':'1', 'cursor':'default'})
+    var upgradeData = $(this).attr('data-name')
     $(this).css('opacity', '0.6')
+
     var upgradableShipParts = $('.one, .two, .three, .four, .five, .six, .seven, .eight')
+
+    $(upgradableShipParts).unbind()
+
     $(upgradableShipParts).mouseenter(function() {
         $(this).css({'opacity':'0.6', 'cursor':'pointer'})
     })
     $(upgradableShipParts).mouseleave(function() {
         $(this).css({'opacity':'1', 'cursor':'default'})
     })
+
     $(upgradableShipParts).click(function() {
+        console.log(upgradeData, this.classList[0], this.classList[1])
         $(this).html('')
         var img = document.createElement('img')
         img.src = upgradeSrc
         $(img).appendTo(this)
+
+        var upgradeSlot = recordTable[this.classList[0]][this.classList[1]][this.classList[2]]
+        upgradeSlot = upgradeData
+        console.log(upgradeSlot)
     })
 })
