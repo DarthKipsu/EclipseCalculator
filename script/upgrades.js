91033fadfbd260eedfacf74e9fdf411da809c94f
Verna Koskinen <darth.kipsu@gmail.com>
2014-05-20 10:56:00 +0300

    fix upgrade hover effects

--- script/upgrades.js
+++ script/upgrades.js
@@ -1,6 +1,7 @@
 $(document).on('click', '.tech > img', function() {
     var upgradeSrc = this.src
     var upgradeData = $(this).attr('data-name')
+    $('.tech > img').css('opacity', '1')
     $(this).css('opacity', '0.6')
 
     var upgradableShipParts = $('.one, .two, .three, .four, .five, .six, .seven, .eight')
