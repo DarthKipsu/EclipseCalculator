2ed9d9e33f8baf7a50c3edd4d68f573b40886add
Verna Koskinen <darth.kipsu@gmail.com>
2014-05-21 21:41:21 +0300

    clean up upgrades.js

--- script/upgrades.js
+++ script/upgrades.js
@@ -1,59 +1,76 @@
 $(document).on('click', '.tech > img', function() {
     var upgradeSrc = this.src
-    var upgradeData = $(this).attr('data-name')
-    $('.tech > img').css('opacity', '1')
-    $(this).css('opacity', '0.6')
-
+    var upgradeName = $(this).attr('data-name')
     var upgradableShipParts = $('.one, .two, .three, .four, .five, .six, .seven, .eight')
 
-    $(upgradableShipParts).unbind()
+    updateHoverEffect(this, upgradableShipParts)
 
+    $(upgradableShipParts).click(function() {
+        upgradeOldPart(this, upgradeSrc, upgradeName)
+    })
+})
+
+function updateHoverEffect(selection, upgradableShipParts) {
+    $('.tech > img').css('opacity', '1')
+    $(selection).css('opacity', '0.6')
+
+    $(upgradableShipParts).unbind()
     $(upgradableShipParts).mouseenter(function() {
         $(this).css({'opacity':'0.6', 'cursor':'pointer'})
     })
     $(upgradableShipParts).mouseleave(function() {
         $(this).css({'opacity':'1', 'cursor':'default'})
     })
+}
 
-    $(upgradableShipParts).click(function() {
-        console.log('upgrade:', upgradeData, 'race:', this.classList[0], 'ship:', this.classList[1], 'slot:', this.classList[2])
-        var upgradeSlot = recordTable[this.classList[0]][this.classList[1]]
-        checkValidity(this.classList, upgradeData, upgradeSlot)
-        $(this).html('')
-        var img = document.createElement('img')
-        img.src = upgradeSrc
-        $(img).appendTo(this)
+function upgradeOldPart(selection, upgradeSrc, upgradeName) {
+    var upgradeSlot = recordTable[selection.classList[0]][selection.classList[1]]
 
-        upgradeSlot.dice1HPmissile = addAttribute('dice1HPmissile', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.dice2HPmissile = addAttribute('dice2HPmissile', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.dice1HP = addAttribute('dice1HP', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.dice2HP = addAttribute('dice2HP', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.dice4HP = addAttribute('dice4HP', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.computer = addAttribute('computer', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.shield = addAttribute('shield', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.hull = addAttribute('hull', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.initiative = addAttribute('initiative', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.energy = addAttribute('energy', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot.speed = addAttribute('speed', this.classList, upgradeData, upgradeSlot)
-        upgradeSlot[this.classList[2]] = upgradeData
-        console.log(recordTable[this.classList[0]][this.classList[1]])
-    })
-})
+    replaceImage(selection, upgradeSrc)
+    updateShipAttributes(selection, upgradeName, upgradeSlot)
+    replaceUpgradeName(upgradeSlot, selection.classList, upgradeName)
 
-function checkValidity(classList, upgradeData, upgradeSlot) {
-    var energy = addAttribute('energy', classList, upgradeData, upgradeSlot)
-    if (energy<0) console.log('NOT ENOUGH ENERGY! (' + energy + ')')
-    if (classList[1]!='starbase') {
-        var speed = addAttribute('speed', classList, upgradeData, upgradeSlot)
-        if (speed<1) console.log('NEED ENGINE!')
+    checkValidity(selection.classList, upgradeSlot)
+    console.log('ship:', recordTable[selection.classList[0]][selection.classList[1]])
+
+}
+
+function replaceImage(selection, upgradeSrc) {
+    $(selection).html('')
+    var img = document.createElement('img')
+    img.src = upgradeSrc
+    $(img).appendTo(selection)
+}
+
+function updateShipAttributes(selection, upgradeName, upgradeSlot) {
+    var attr = ['dice1HPmissile', 'dice2HPmissile', 'dice1HP', 'dice2HP', 'dice4HP',
+        'computer', 'shield', 'hull', 'initiative', 'energy', 'speed']
+
+    for (var i=0; i<attr.length; i++) {
+        upgradeSlot[attr[i]] = addAttribute(attr[i], selection.classList, upgradeName, upgradeSlot)
     }
 }
 
-function addAttribute(attribute, classList, upgradeData, upgradeSlot) {
+function addAttribute(attribute, classList, upgradeName, upgradeSlot) {
     var currentAttribute = upgradeSlot[attribute]
-    if (upgradeAttributes[upgradeSlot[classList[2]]]) {
-        var removedUpgrade = upgradeAttributes[upgradeSlot[classList[2]]][attribute]
-    } else var removedUpgrade = 0
-    var newUpgrade = upgradeAttributes[upgradeData][attribute]
+    var removedUpgrade = getRemovedAttribute(attribute, classList, upgradeSlot)
+    var newUpgrade = upgradeAttributes[upgradeName][attribute]
     return currentAttribute - removedUpgrade + newUpgrade
+}
+
+function getRemovedAttribute(attribute, classList, upgradeSlot) {
+    if (upgradeAttributes[upgradeSlot[classList[2]]]) {
+        return upgradeAttributes[upgradeSlot[classList[2]]][attribute]
+    } else return 0
+}
+
+function replaceUpgradeName(upgradeSlot, classList, upgradeName) {
+    upgradeSlot[classList[2]] = upgradeName
+}
+
+function checkValidity(classList, upgradeSlot) {
+    if (upgradeSlot.energy<0) console.log('NOT ENOUGH ENERGY! (' + upgradeSlot.energy + ')')
+    if (classList[1]!='starbase') {
+        if (upgradeSlot.speed<1) console.log('NEED ENGINE!')
+    }
 }
