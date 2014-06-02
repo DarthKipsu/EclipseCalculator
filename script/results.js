8dbc1e4ecac64feb5f0c936184aa149634c5774e
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-02 19:49:13 +0300

    add function to count the ships in play

--- script/results.js
+++ script/results.js
@@ -6,7 +6,29 @@ function allShipsAreValid() {
 
 function showResults() {
     addContentFromHTML('#results', 'results.html')
+    addResultsCloseX()
+
+    var shipsAttending = countTheShips()
+
+    var initiativeOrder = []
+    
+}
+
+function addResultsCloseX() {
     $('#results').on('click', '.close', function() {
         $('#results').hide()
     })
+}
+
+function countTheShips() {
+    var inputs = document.getElementsByTagName('input')
+    var shipsAttending = []
+    for (var i=0; i<inputs.length; i++) {
+        if (inputs[i].value>0) {
+            for (var j=0; j<inputs[i].value; j++) {
+                shipsAttending.push([inputs[i].classList[0], inputs[i].name])
+            }
+        }
+    }
+    return shipsAttending
 }
