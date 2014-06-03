44f58e977b4c4d8e221090f7ffe29d113cce4ba1
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-03 21:07:19 +0300

    add specs for reordering ships in initiative order

--- specs/results.spec.js
+++ specs/results.spec.js
@@ -1,14 +1,52 @@
 describe('adding ships in initiative order', function() {
 
+    var inputs
+
+    beforeEach(function() {
+        inputs = [{value: 2, name: 'interceptor', classList: ['player']},
+            {value: 2, name: 'cruiser', classList: ['player']},
+            {value: 0, name: 'dreadnought', classList: ['player']},
+            {value: 0, name: 'starbase', classList: ['player']},
+            {checked: true},
+            {value: 1, name: 'interceptor', classList: ['enemy']},
+            {value: 3, name: 'cruiser', classList: ['enemy']},
+            {value: 1, name: 'dreadnought', classList: ['enemy']},
+            {value: 1, name: 'starbase', classList: ['enemy']}]
+    })
+
     it('adds the right ship types in game', function() {
-        var inputs = [{value: 2, name: 'interceptor', classList: ['player']},
-            {value: 1, name: 'interceptor', classList: ['enemy']}]
-        var player = 'attacker'
-        var enemy = 'defender'
-        var shipsAttending = addShipsBasedOnInputs(inputs, player, enemy)
-        expect(shipsAttending.length).toBe(3)
+        var shipsAttending = addShipsBasedOnInputs(inputs, 'attacker', 'defender')
+        expect(shipsAttending.length).toBe(10)
         expect(shipsAttending[0]).toEqual(shipsAttending[1])
         expect(shipsAttending[0]).not.toEqual(shipsAttending[2])
+    })
+
+    it('adds enemyTerran defending interceptors before terran players attacking ones', function() {
+        chosenRace = 'terran'
+        enemyRace = 'enemyTerran'
+        var shipsAttending = addShipsBasedOnInputs(inputs, 'attacker', 'defender')
+        var initiativeOrder = []
+        while (shipsAttending.length>0) {
+            moveTheShipWithBiggestInitiative(shipsAttending, initiativeOrder)
+        }
+        expect(initiativeOrder[1][0].type).toBe('interceptor')
+        expect(initiativeOrder[1][1]).toBe('defender')
+        expect(initiativeOrder[2][0].type).toBe('interceptor')
+        expect(initiativeOrder[2][1]).toBe('attacker')
+    })
+
+    it('adds planta starbase only after attacking terran interceptors', function() {
+        chosenRace = 'terran'
+        enemyRace = 'planta'
+        var shipsAttending = addShipsBasedOnInputs(inputs, 'attacker', 'defender')
+        var initiativeOrder = []
+        while (shipsAttending.length>0) {
+            moveTheShipWithBiggestInitiative(shipsAttending, initiativeOrder)
+        }
+        expect(initiativeOrder[1][0].type).toBe('interceptor')
+        expect(initiativeOrder[1][1]).toBe('attacker')
+        expect(initiativeOrder[2][0].type).toBe('starbase')
+        expect(initiativeOrder[2][1]).toBe('defender')
     })
 
 })
