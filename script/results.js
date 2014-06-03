6637dd175f463f50dbf50bc8a6972bee03ec4932
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-03 19:48:57 +0300

    clean up ship reordering

--- script/results.js
+++ script/results.js
@@ -8,31 +8,7 @@ function showResults() {
     addContentFromHTML('#results', 'results.html')
     hideResultsWithX()
 
-    var shipsAttending = countTheShips()
-    if (shipsAttending.length==0) console.log('no ships selected!')
-
-    var initiativeOrder = []
-    while (shipsAttending.length>0) {
-        var biggestInitiative = [{initiative: -1}]
-        var index
-        for (i=0; i<shipsAttending.length; i++) {
-            if (shipsAttending[i][0].initiative>biggestInitiative[0].initiative) {
-                biggestInitiative = shipsAttending[i]
-                index = i
-                console.log(biggestInitiative[0].initiative)
-            } else if (shipsAttending[i][0].initiative==biggestInitiative[0].initiative &&
-                       shipsAttending[i][1]=='defender') {
-                biggestInitiative = shipsAttending[i]
-                index = i
-                console.log('defender')
-            }
-        }
-        console.log('push')
-        initiativeOrder.push(biggestInitiative)
-        shipsAttending.splice(index,1)
-        biggestInitiative = [{initiative: -1}]
-    }
-    console.log('init order:', initiativeOrder)
+    var initiativeOrder = createInitiativeOrder()
 }
 
 function hideResultsWithX() {
@@ -41,24 +17,56 @@ function hideResultsWithX() {
     })
 }
 
+function createInitiativeOrder() {
+    var shipsAttending = countTheShips()
+    if (shipsAttending.length==0) console.log('no ships selected!')
+
+    var initiativeOrder = []
+    while (shipsAttending.length>0) {
+        moveTheShipWithBiggestInitiative(shipsAttending, initiativeOrder)
+    }
+    console.log('init order:', initiativeOrder)
+    return initiativeOrder
+}
+
 function countTheShips() {
     var inputs = document.getElementsByTagName('input')
+    var player = inputs[4].checked?'attacker':'defender'
+    var enemy = inputs[4].checked?'defender':'attacker'
+    return addShipsBasedOnInputs(inputs, player, enemy)
+}
+
+function addShipsBasedOnInputs(inputs, player, enemy) {
     var shipsAttending = []
-    var player = 'defender'
-    var enemy = 'attacker'
-    if (inputs[4].checked) {
-        player = 'attacker'
-        enemy = 'defender'
-    }
-    console.log(player)
     for (var i=0; i<inputs.length; i++) {
         if (inputs[i].value>0) {
-            for (var j=0; j<inputs[i].value; j++) {
-                if (inputs[i].classList[0]=='player') {
-                    shipsAttending.push([recordTable[chosenRace][inputs[i].name], player])
-                } else shipsAttending.push([recordTable[enemyRace][inputs[i].name], enemy])
-            }
+            addAsManyShipsAsInputValue(inputs, player, enemy, i, shipsAttending)
         }
     }
     return shipsAttending
+}
+
+function addAsManyShipsAsInputValue(inputs, player, enemy, i, shipsAttending) {
+    for (var j=0; j<inputs[i].value; j++) {
+        if (inputs[i].classList[0]=='player') {
+            shipsAttending.push([recordTable[chosenRace][inputs[i].name], player])
+        } else shipsAttending.push([recordTable[enemyRace][inputs[i].name], enemy])
+    }
+}
+
+function moveTheShipWithBiggestInitiative(shipsAttending, initiativeOrder) {
+    var biggestInitiative = [{initiative: -1}]
+    var index
+    for (i=0; i<shipsAttending.length; i++) {
+        if (shipsAttending[i][0].initiative>biggestInitiative[0].initiative) {
+            biggestInitiative = shipsAttending[i]
+            index = i
+        } else if (shipsAttending[i][0].initiative==biggestInitiative[0].initiative &&
+                   shipsAttending[i][1]=='defender') {
+            biggestInitiative = shipsAttending[i]
+            index = i
+        }
+    }
+    initiativeOrder.push(biggestInitiative)
+    shipsAttending.splice(index,1)
 }
